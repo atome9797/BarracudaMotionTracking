89b74719415814913eeb0d4a5279f99e12ccdf07
atome9797 <65073631+atome9797@users.noreply.github.com>
2022-10-18 18:24:34 +0900

    다시 테스트

---
+++ AvProTestPlusVimeo/Assets/Scripts/InputManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour
+{
+    public void LoadSceneRecord()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}

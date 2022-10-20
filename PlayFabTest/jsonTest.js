20b8a6269cdf7ec92e4a6d85864ca05d85fe7a3b
atome9797 <65073631+atome9797@users.noreply.github.com>
2022-10-20 15:49:04 +0900

    json 정렬 알고리즘 추가

--- PlayFabTest/jsonTest.js
+++ PlayFabTest/jsonTest.js
@@ -168,3 +168,31 @@ searchData(20221021153018392, 3, 1);
 console.log(array);
 
 
+//json 내림차순, 오름차순 정렬
+
+var sortJSON = function(data, key, type) {
+  if (type == undefined) {
+    type = "asc";
+  }
+  return data.sort(function(a, b) {
+    var x = a[key];
+    var y = b[key];
+    if (type == "desc") {
+      return x > y ? -1 : x < y ? 1 : 0;
+    } else if (type == "asc") {
+      return x < y ? -1 : x > y ? 1 : 0;
+    }
+  });
+};
+
+var testData = [{ key: 2 }, { key: 1 }, { key: 3 }, { key: 5 }, { key: 4 }];
+
+console.log(testData);
+// [ { key: 2 }, { key: 1 }, { key: 3 }, { key: 5 }, { key: 4 } ]
+
+// 오름차순
+console.log(sortJSON(testData, "key", "asc"));
+// [ { key: 1 }, { key: 2 }, { key: 3 }, { key: 4 }, { key: 5 } ]
+
+// 내림차순
+console.log(sortJSON(testData, "key", "desc"));
